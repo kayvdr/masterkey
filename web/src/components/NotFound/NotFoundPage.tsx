14402d25805ca76dfe4ecb02ad3a4bead6e9da9c
kay <kay.vieider@hgv.it>
2023-02-28 19:40:54 +0100

    Add 404 Page

--- web/src/components/NotFound/NotFoundPage.tsx
+++ web/src/components/NotFound/NotFoundPage.tsx
@@ -1,9 +1,26 @@
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
+import Button from "../../ui/Button";
+import Footer from "../Home/Footer";
+import Header from "../Home/Header";
 import styles from "../NotFound/NotFoundPage.module.css";
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <h1 className={styles.title}>404 Not Found</h1>
+      <Header />
+      <section className={styles.sectionGrey}>
+        <div className="container">
+          <h1 className={styles.title}>Error 404</h1>
+        </div>
+      </section>
+      <section className={classNames("container", styles.wrapper)}>
+        <p>The Page was not found</p>
+        <Button onClick={() => navigate("/")}>Back to Home</Button>
+      </section>
+      <Footer />
     </>
   );
 };
