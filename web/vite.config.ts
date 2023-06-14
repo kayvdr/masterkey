0958dcb218c26ac6116abf9c1a5c23f6449059ab
kay <kay.vieider@hgv.it>
2023-06-14 22:56:15 +0200

    Add supabase to env

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,15 +4,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: { sourcemap: true },
+  envDir: "..",
   server: {
     port: 60005,
     strictPort: true,
-    proxy: {
-      "/__api": {
-        target: "http://localhost:60001",
-        changeOrigin: true,
-      },
-    },
   },
   plugins: [react()],
 });
