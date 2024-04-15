7674142d53961acdceb7d21960ad32a992af37d4
on3k <on3k.dev@gmail.com>
2024-04-15 21:26:06 +0200

    Fix supbase auth

--- web/src/hooks/useAuth.ts
+++ web/src/hooks/useAuth.ts
@@ -1,17 +1,21 @@
-import { Session } from "@supabase/supabase-js";
+import { AuthError, Session } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 import { supabase } from "../http/supabase";
 
 const useAuth = () => {
   const [currentSession, setSession] = useState<Session | null>(null);
+  const [error, setError] = useState<AuthError | null>(null);
 
   const handleSession = async () => {
-    const {
-      data: { session },
-      error,
-    } = await supabase.auth.getSession();
-
-    !error && setSession(session);
+    supabase.auth
+      .getSession()
+      .then(({ data: { session } }) => {
+        setSession(session);
+      })
+      .catch((error) => {
+        console.error(error);
+        setError(error);
+      });
   };
 
   useEffect(() => {
@@ -20,7 +24,7 @@ const useAuth = () => {
 
   supabase.auth.onAuthStateChange(handleSession);
 
-  return { session: currentSession };
+  return { session: currentSession, error };
 };
 
 export default useAuth;
