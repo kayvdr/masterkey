9352c1cf0a984a9ca2e649e630e0b4899c6a77a8
kayvdr <kayvieider97@gmail.com>
2024-12-31 16:46:35 +0100

    Change Elevenlabs

--- web/src/components/icons/Elevenlabs.tsx
+++ web/src/components/icons/Elevenlabs.tsx
@@ -1,8 +1,11 @@
 import { Glyph } from "../../types";
 
 const SvgElevenlabs: Glyph = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 180 292" {...props}>
-    <path fill="#000" d="M120 0h60v292h-60V0zM0 0h60v292H0V0z" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 340 292" {...props}>
+    <path
+      d="m199.85721,0.07143l60,0l0,292l-60,0l0,-292zm-120,0l60,0l0,292l-60,0l0,-292z"
+      fill="#000"
+    />
   </svg>
 );
 
