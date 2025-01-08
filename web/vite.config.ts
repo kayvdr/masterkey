70847790ca4cc25be0d321eb2c96c0b1d9f4c1c6
kayvdr <kayvieider97@gmail.com>
2025-01-08 22:04:15 +0100

    Add favicon

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,39 +1,25 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig, UserConfigExport } from "vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const baseConfig: UserConfigExport = {
-    build: { sourcemap: true },
-    envDir: "..",
-    css: {
-      devSourcemap: true,
-      modules: {
-        generateScopedName: "[hash:base64:8]",
+export default defineConfig(() => ({
+  build: { sourcemap: true },
+  envDir: "..",
+  css: {
+    devSourcemap: true,
+    modules: {
+      generateScopedName: "[hash:base64:8]",
+    },
+  },
+  server: {
+    port: 60000,
+    strictPort: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:60001",
+        changeOrigin: true,
       },
     },
-    server: {
-      port: 60000,
-      strictPort: true,
-      proxy: {
-        "/api": {
-          target: "http://localhost:60001",
-          changeOrigin: true,
-        },
-      },
-    },
-    plugins: [react()],
-  };
-
-  // Use different favicon in dev environment
-  if (command === "serve") {
-    const config: UserConfigExport = {
-      ...baseConfig,
-      publicDir: "public-dev",
-    };
-
-    return config;
-  }
-
-  return baseConfig;
-});
+  },
+  plugins: [react()],
+}));
